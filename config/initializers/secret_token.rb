0712e454e9ea3426b07b6b9826e92206afec395e
asem <asem0525@gmail.com>
2013-06-27 18:00:23 +0100

    Created a rails application and status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kazakhbook::Application.config.secret_token = '29da7e121025c8bbfda05600f7eb5e39edeb982f4a7efca0e820b96690924eb510cd8b2cea474339f4d18ce08fdbf34424e4146549d6e189daa351a9ad0db661'
